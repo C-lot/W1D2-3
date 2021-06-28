ee777afe6a476d11ac09fb509bb7a110cf9bb17f
C-lot <73837008+C-lot@users.noreply.github.com>
2021-06-28 09:25:46 +0200

    Add files via upload

---
+++ W1D2-3/script.js
@@ -0,0 +1,10 @@
+console.log("Hallo Winc Academy studenten");
+
+//dit is een grote som (visable)
+
+console.log(1230941 * 1823794);
+
+/*dit is een block comment
+console.log(637263 / 54);*/
+
+//not visible
