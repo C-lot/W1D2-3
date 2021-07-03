db2b652b156f36b6caf46e470e4b3b257f7567ed
C-lot <73837008+C-lot@users.noreply.github.com>
2021-07-03 13:29:16 +0200

    Add files via upload

---
+++ W1D2-3/Assignment_17_forLoops/grade-assigner.js
@@ -0,0 +1,13 @@
+for (let i = 60; i <= 100; i++) {
+  if (i > 90) {
+    console.log(`For scoring ${i} you get A`);
+  } else if (i > 80) {
+    console.log(`For scoring ${i} you get B`);
+  } else if (i > 70) {
+    console.log(`For scoring ${i} you get C`);
+  } else if (i > 65) {
+    console.log(`For scoring ${i} you get D`);
+  } else {
+    console.log(`For scoring ${i} you get E`);
+  }
+}
---
+++ W1D2-3/Assignment_17_forLoops/multiplication-tables.js
@@ -0,0 +1,12 @@
+for (let i = 1; i <= 10; i++) {
+  const table9 = i * 9;
+  console.log(`${i} x 9 = ${table9}`);
+}
+
+//Bonus
+for (let i = 1; i <= 10; i++) {
+  for (let j = 1; j < 10; j++) {
+    const total = i * j;
+    console.log(`${i} x ${j} = ${total}`);
+  }
+}
